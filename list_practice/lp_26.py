ba2262ee2de1f60b3c2a404c1a5dc5590f0aeb11
arjun5781093 <arjun5781093@gmail.com>
2024-09-15 09:23:41 +0530

    Updated lp_26.py circularly identical program

--- list_practice/lp_26.py
+++ list_practice/lp_26.py
@@ -1,32 +1,5 @@
-#!/usr/bin/python3
-
-#A program to find whether two lists are cirucularly identical or not
-
 l1=list(map(int,input("Enter list-1: ").split()))
 l2=list(map(int,input("Enter list-2: ").split()))
-'''l1=[1,2,3,4]
-l2=[2,1,3,4]'''
-
-#Edge cases
-if len(l1) != len(l2):
-    print("False")
-elif l1==l2 and l1!=[]:
-    print("True")
-elif l1==l2==[]:
-    print("True")
-else:
-    j=l2.index(l1[0])
-    i=0
-    while i < len(l1):
-        if l1[i] != l2[j]:
-            print("False")
-            break
-        else:
-            i += 1
-            j = (j+1)%len(l2)
-    else:
-        print("True")
-
 
 def find_index(ele,li):
 #Return list of indices of occurences of element in list
@@ -37,22 +10,25 @@ def find_index(ele,li):
             l.append(i)
     return l
 
-'''
-ind_l = find_index(l1[0],l2)
-print(ind_l)
-if ind_l:
-    for ind in ind_l:
+if len(l1) != len(l2):
+    print("False")
+elif l1==l2 and l1!=[]:
+    print("True")
+elif l1==l2==[]:
+    print("True")
+else:
+    ind_l = find_index(l1[0],l2)
+    for each in ind_l:
+        j=each
         i=0
-        j=ind
-        while i<len(l1) and  l1[i] == l2[j]:
-            i+=1
-            j = (j+1)%len(l2)
-        if i > len(l1):
+        while i < len(l1):
+            if l1[i] != l2[j]:
+                break
+            else:
+                i += 1
+                j = (j+1)%len(l2)
+        else:
             print("True")
             break
-        else:
-            print("False")
-            break
-else:
-    print("False")
-'''
+    else:
+        print("False")
