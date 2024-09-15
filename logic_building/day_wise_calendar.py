7ea6cf2f29ff6518bee42fe6905c1e0e8c8e54c4
arjun5781093 <arjun5781093@gmail.com>
2024-09-15 17:56:42 +0530

    Modified day_wise_calendar.py

--- logic_building/day_wise_calendar.py
+++ logic_building/day_wise_calendar.py
@@ -1,13 +1,14 @@
 #A program to print daywise calendar, given first day of the month and number of days in month
+#Time Complexity: O(31)
+#Space Complexity: O(1)
 day=input("Enter first day of the month: ")
 n=int(input("Enter number of days in month: "))
 days_of_week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
 code = days_of_week.index(day)
-#print(code)
 #First line
 print("Su Mo Tu We Th Fr Sa")
-print('   '*code,end="")
+print('   '*code,end="") #printing space in the first line
 for i in range(1,(7-code)+1):
     print(i,end="  ")
 
@@ -15,11 +16,11 @@ print()
 #From second line
 count=0
 for j in range((7-code)+1,n+1):
-    if count == 7:
+    if count == 7: #To print 7 numbers in one line
         print()
         count=0
-    if j >9:
+    if j >9: #if number to be printed is of two digits, print only one space
         print(j,end=" ")
-    else:
+    else: #if number to be printed is of one digit, print two spaces
         print(j,end="  ")
     count+=1
